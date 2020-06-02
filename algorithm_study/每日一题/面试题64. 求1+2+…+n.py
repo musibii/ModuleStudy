48865892f756be68029090859c4f84bb461ec6b9
musibii <musibii@wemart.cn>
2020-06-02 23:17:55 +0800

    短路跳出递归

---
+++ algorithm_study/每日一题/面试题64. 求1+2+…+n.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+# __author__: musibii
+# __file__  : 面试题64. 求1+2+…+n.py
+# __time__  : 2020/6/2 7:53 上午
+
+import typing
+
+"""
+求 1+2+...+n ，要求不能使用乘除法、for、while、if、else、switch、case等关键字及条件判断语句（A?B:C）。
+
+ 
+
+示例 1：
+
+输入: n = 3
+输出: 6
+示例 2：
+
+输入: n = 9
+输出: 45
+ 
+
+限制：
+
+1 <= n <= 10000
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/qiu-12n-lcof
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+
+"""
+class Solution:
+    def __init__(self):
+        self.res = 0
+    def sumNums(self, n: int) -> int:
+        n > 1 and self.sumNums(n - 1)
+        self.res += n
+        return self.res
+
+
