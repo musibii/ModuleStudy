a2d708ce0e6f3eac8f18797f36ab2270bdf49551
ukyzr <shaozuanzuan@gmail.com>
2020-06-27 23:51:43 +0800

    322. 零钱兑换 备忘录

--- algorithm_study/LeetCode/322. 零钱兑换.py
+++ algorithm_study/LeetCode/322. 零钱兑换.py
@@ -31,4 +31,27 @@ import typing
 
 class Solution:
     def coinChange(self, coins: typing.List[int], amount: int) -> int:
-        ""
+        memo = dict()
+
+        def dp(n):
+            if n in memo:
+                return memo[n]
+            if n == 0:
+                return 0
+            if n < 1:
+                return -1
+            res = float("INF")
+            for coin in coins:
+                subproblem = dp(n - coin)
+                if subproblem == -1:
+                    continue
+                res = min(res, 1 + subproblem)
+            memo[n] = res if res != float("INF") else -1
+
+            return memo[n]
+
+        return dp(amount)
+
+
+if __name__ == "__main__":
+    print(Solution().coinChange([1, 2, 5], 100))
